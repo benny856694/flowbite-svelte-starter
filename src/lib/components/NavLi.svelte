c4a9b2be5cd5cd74339f30e50cf28e6c1b38dff9
BinShen <shenbinsc@gmail.com>
2023-04-25 17:44:16 +0800

    simplify

--- src/lib/components/NavLi.svelte
+++ src/lib/components/NavLi.svelte
@@ -1,23 +1,12 @@
 <script lang="ts">
     import classNames from 'classnames';
     import { afterUpdate, getContext } from 'svelte';
-    import type { NavbarLiType } from './NavUl.svelte';
   
     export let href: string = '';
     export let active: boolean = false;
-    export let activeClass: string | undefined = undefined;
-    export let nonActiveClass: string | undefined = undefined;
-
     export let data: string = '';
   
-    const context = getContext<NavbarLiType>('navbar') ?? {};
-  
-    let liClass: string;
-    $: liClass = classNames(
-      'block py-2 pr-4 pl-3 md:p-0 rounded md:border-0',
-      active ? activeClass ?? context.activeClass : nonActiveClass ?? context.nonActiveClass,
-      $$props.class
-    );
+ 
 
     afterUpdate(() => {
         console.log(`${data} updated, active: ${active}`);
@@ -32,18 +21,8 @@
     <svelte:element
       this={href ? 'a' : 'div'}
       {href}
-      {...$$restProps}
-      on:blur
-      on:change
-      on:click
-      on:focus
-      on:keydown
-      on:keypress
-      on:keyup
-      on:mouseenter
-      on:mouseleave
-      on:mouseover
-      class={liClass}>
+   
+      >
       <slot />
     </svelte:element>
   </li>
